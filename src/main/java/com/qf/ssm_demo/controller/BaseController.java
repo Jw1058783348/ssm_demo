dfdd5a21ff30d83f4edadb4984e1826b8c4c2213
JiangWei <jiangwei1058783348@gmail.com>
2020-06-01 16:43:51 +0800

    测试ssm默认静态文件夹并得出结论 (结论位于BaseController)

--- src/main/java/com/qf/ssm_demo/controller/BaseController.java
+++ src/main/java/com/qf/ssm_demo/controller/BaseController.java
@@ -11,8 +11,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BaseController {
 
-    @RequestMapping("/index")
-    public String toIndex(){
-        return "index";
+    @RequestMapping("/index1")
+    public String toIndex(){//可以通过controller访问到templates里面的页面,不可以直接通过路径访问(静态资源路径未指定为template)
+        return "index1";//指定resorce后可以直接通过路径访问
     }
+    @RequestMapping("/index2")//不可以通过controller访问到templates里面的页面,不可以直接通过路径访问(静态资源路径未指定为static)
+    public String toIndex2(){
+        return "index2";//指定后可以直接通过路径访问，但是不能通过controller
+    }
+    @RequestMapping("/index3")//不可以通过controller访问到templates里面的页面,可以直接通过路径访问((静态资源路径未指定为webapp:localhost:8080/index3.html)
+    public String toIndex3(){
+        return "index3";//指定后可以直接通过路径访问，但是不能通过controller
+    }
+    //结论:经过application.yml指定路径之后可以访问到静态资源文件夹,可直接访问的静态资源文件夹是指定的或者webapp，除了templates文件夹以外其他文件夹无法通过controller实现跳转
 }
