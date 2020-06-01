f17bc440aa24af78230e57b20c3f323fffa647ff
JiangWei <jiangwei1058783348@gmail.com>
2020-06-01 11:02:34 +0800

    打包成功版本2.0(支持前后端分离部署)

--- src/main/java/com/qf/ssm_demo/SsmDemoApplication.java
+++ src/main/java/com/qf/ssm_demo/SsmDemoApplication.java
@@ -2,15 +2,22 @@ package com.qf.ssm_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 //@Import(org.apache.poi.hssf.usermodel.HSSFWorkbook.class)
-public class SsmDemoApplication {
+public class SsmDemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
 
         SpringApplication.run(SsmDemoApplication.class, args);
 
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(SsmDemoApplication.class);
     }
 
    /* @Bean
